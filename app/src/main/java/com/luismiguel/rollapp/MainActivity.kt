10170a5e9ce54631fc8ade29e54214cf29887571
mcasasq@gmail.com <mcasasq@gmail.com>
2020-09-01 17:40:28 -0500

    agregar ImageView

--- app/src/main/java/com/luismiguel/rollapp/MainActivity.kt
+++ app/src/main/java/com/luismiguel/rollapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.luismiguel.rollapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -19,10 +20,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        //Varaible resultText de tipo TextView
-        val resultText: TextView = findViewById(R.id.result_text)
-
         val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val rollText: TextView = findViewById(R.id.result_text)
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
+        rollText.text = randomInt.toString()
     }
 }
