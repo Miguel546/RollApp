d5207f5454c88c1af1424f46e5985cde2cae4fe4
mcasasq@gmail.com <mcasasq@gmail.com>
2020-09-01 17:08:18 -0500

    dice roll app version 1

---
+++ app/src/main/java/com/luismiguel/rollapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.luismiguel.rollapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //Variable rollButton de tipo Button
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener({
+            rollDice()
+        })
+    }
+
+    private fun rollDice() {
+        //Varaible resultText de tipo TextView
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        val randomInt = Random().nextInt(6) + 1
+        resultText.text = randomInt.toString()
+    }
+}
